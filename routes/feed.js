5ccc3f4ecdd2b9048ac863f4536d8bfd63921d51
user <user@users-MacBook-Pro.local>
2022-06-21 11:04:55 +0100

    Added pagination

--- routes/feed.js
+++ routes/feed.js
@@ -23,12 +23,15 @@ router.get("/post/:postId", feedController.getPost);
 
 // PUT /feed/post/:postId
 router.put(
-  "post/:postId",
+  "/post/:postId",
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
   feedController.updatePost
 );
+
+// DELETE /feed/post/:postId
+router.delete("/post/:postId", feedController.deletePost);
 
 module.exports = router;
