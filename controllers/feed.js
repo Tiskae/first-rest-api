0b22047aa089ff6f5a288d1ea75c2057777d872f
user <user@users-MacBook-Pro.local>
2022-06-21 08:15:07 +0100

    Added editing post and image upload

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
@@ -30,13 +33,19 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422; // validation error
     throw error;
   }
+  if (!req.file) {
+    const fileError = new Error("No image provided!");
+    fileError.statusCode = 422;
+    throw fileError;
+  }
 
   const { title, content } = req.body;
+  const imageUrl = req.file.path;
   const post = new Post({
     title,
     content,
     creator: { name: "Eniola" },
-    imageUrl: "images/view.jpeg",
+    imageUrl,
   });
 
   post
@@ -76,4 +85,61 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation error");
+    error.statusCode = 422;
+    throw err;
+  }
+
+  const { title, content } = req.body;
+  let imageUrl = req.body.image;
+
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+  if (!imageUrl) {
+    const error = new Error("No file picked!");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const postError = new Error("Could not find post");
+        postError.statusCode = 404;
+        throw postError;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+
+      return post.save();
+    })
+    .then((result) => {
+      res
+        .status(200)
+        .json({ message: "Post updated succesffully!", post: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = (filePath) => {
+  const fPath = path.join(__dirname, "..", filePath);
+  fs.unlink(fPath, console.error);
 };
--- routes/feed.js
+++ routes/feed.js
@@ -12,17 +12,23 @@ router.get("/posts", feedController.getPosts);
 router.post(
   "/post",
   [
-    body("title", "title must be at least 5 characters long string")
-      .trim()
-      .isLength({ min: 5 }),
-    body("content", "content must be at least 5 characters long string")
-      .trim()
-      .isLength({ min: 5 }),
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
   ],
   feedController.createPost
 );
 
 // GET /feed/post/:postId
 router.get("/post/:postId", feedController.getPost);
+
+// PUT /feed/post/:postId
+router.put(
+  "post/:postId",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.updatePost
+);
 
 module.exports = router;
